80a719ccd95af84e541147a8a8514634b6205830
vuchint <vuchint.sutapalli@gmail.com>
2024-08-19 15:08:03 +0530

    styling

--- Client/vite-project/src/components/LatestBlock.jsx
+++ Client/vite-project/src/components/LatestBlock.jsx
@@ -7,7 +7,7 @@ const LatestBlockComponent = () => {
   if (!latestBlock) {
     return (
       <div className="flex justify-center items-center h-64 bg-white rounded-lg shadow-md">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-indigo-500"></div>
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-indigo-500" />
       </div>
     );
   }
--- Client/vite-project/src/components/RecentBlocksPreview/index.jsx
+++ Client/vite-project/src/components/RecentBlocksPreview/index.jsx
@@ -16,7 +16,7 @@ const RecentBlocks = () => {
   if (!recentBlocks) {
     return (
       <div className="flex justify-center items-center h-64 bg-white rounded-lg shadow-md">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-indigo-500"></div>
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-indigo-500" />
       </div>
     );
   }
